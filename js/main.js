44974b54017867af413be45c0708826e96174c0a
Carlos Alcalá <CAlcala@audalia.com>
2016-06-21 18:01:19 +0200

    Adding content to the sections. Some style

--- js/main.js
+++ js/main.js
@@ -1,14 +1,53 @@
+// jQuery Variables
 var
-    $panelAbout = $(".ca-aboutPanel") ,
-    $pnlButton  = $(".panelButton"),
-    $body       = $('body')
+    $panelContent   = $(".ca-contentGeneral") ,
+    $pnlButton      = $(".panelButton"),
+    $body           = $('body'),
+    $window         = $(window),
+    $sectionNav     = $('.sectionNav'),
+    $menuIntems     = $('.sectionNav a')
 
+
+//Global variables
+var
+    menuScrollTop   = Number.MAX_VALUE, // Initialize as MAX value in order to not show the menu when the panel is opening
+
+    currentScrollTop,
+    isPanelVisible  = false,
+    sectionToNavigate
+
+
+
+
+console.log(menuScrollTop)
+
+$window.scroll( showHideMenu )
+$menuIntems.on("click",navigateToSection)
 
 $(".panelButton").on("click", function(){
-    $panelAbout.stop().slideToggle(400, function(){
+    $panelContent.stop().slideToggle(400, function(){
         $pnlButton.toggleClass("rotated")
-        $body.animate({scrollTop: $("#ca-aboutMe").offset().top}, 1000)
+        $body.animate({scrollTop: $("#aboutMe").offset().top}, 1000,
+                            function(){
+                                $sectionNav.stop().fadeIn(600)
+                                menuScrollTop = $body.scrollTop()
+                            })
+
     })
 
-
 })
+
+function showHideMenu (){
+    currentScrollTop = $window.scrollTop()
+    if(currentScrollTop < menuScrollTop){
+        $sectionNav.stop().fadeOut(400)
+    }else{
+         $sectionNav.stop().fadeIn(600)
+    }
+}
+
+function navigateToSection (event) {
+    event.preventDefault()
+    sectionToNavigate = $(this).attr("href")
+    $body.stop().animate({scrollTop: $(sectionToNavigate).offset().top}, 800)
+}
